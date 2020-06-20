8fd6c714f0421c8764471a2d094ed2b643fada25
Matthew Reetz <matt@headway.io>
2020-06-20 13:52:33 -0500

    Require admin to view libraries

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,14 +1,14 @@
 import { useAuth } from '@redwoodjs/auth'
 
 const HomePage = () => {
-  const { logIn } = useAuth()
+  const { logIn, logOut, isAuthenticated } = useAuth()
 
   return (
     <div>
       <h1>HomePage</h1>
       <p>Find me in ./web/src/pages/HomePage/HomePage.js</p>
-      <a href="#" onClick={logIn}>
-        Log In
+      <a href="#" onClick={isAuthenticated ? logOut : logIn}>
+        {isAuthenticated ? 'Log Out' : 'Log In'}
       </a>
     </div>
   )

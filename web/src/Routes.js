293e5767e519581c1f0b70161c33300c3d9f8541
Matthew Reetz <matt@headway.io>
2020-06-20 09:54:21 -0500

    Move libraries to admin routes

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -12,10 +12,22 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
-      <Route path="/libraries/new" page={NewLibraryPage} name="newLibrary" />
-      <Route path="/libraries/{id:Int}/edit" page={EditLibraryPage} name="editLibrary" />
-      <Route path="/libraries/{id:Int}" page={LibraryPage} name="library" />
-      <Route path="/libraries" page={LibrariesPage} name="libraries" />
+      <Route
+        path="/admin/libraries/new"
+        page={NewLibraryPage}
+        name="newLibrary"
+      />
+      <Route
+        path="/admin/libraries/{id:Int}/edit"
+        page={EditLibraryPage}
+        name="editLibrary"
+      />
+      <Route
+        path="/admin/libraries/{id:Int}"
+        page={LibraryPage}
+        name="library"
+      />
+      <Route path="/admin/libraries" page={LibrariesPage} name="libraries" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
